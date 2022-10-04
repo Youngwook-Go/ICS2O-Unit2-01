406000477b8b6099b108b3c7e9efc31d10bf959e
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-04 22:21:52 +0000

    a

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) Youngwook All rights reserved
+//
+// Created by: Youngwook
+// Created on: Oct 2022
+// This file contains the JS functions for index1.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
